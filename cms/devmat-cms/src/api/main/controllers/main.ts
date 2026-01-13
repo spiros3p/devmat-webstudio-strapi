4c9ac9048fb7aa7eb271ecc30bb35f601fd84faa
spiros3p <spiros3p@gmail.com>
2026-01-13 12:04:33 +0200

    feat(cms): model changes

--- cms/devmat-cms/src/api/main/controllers/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * main controller
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController("api::main.main");
--- cms/devmat-cms/src/api/main/services/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * main service
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreService("api::main.main");
