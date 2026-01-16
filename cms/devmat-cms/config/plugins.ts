8837880a637e3c8398716801093c9c34ad7c48ed
spiros3p <spiros3p@gmail.com>
2026-01-16 04:12:52 +0200

    feat(cms): add plugin - populate protector

--- cms/devmat-cms/config/plugins.ts
+++ cms/devmat-cms/config/plugins.ts
@@ -2,4 +2,10 @@ export default () => ({
   "webp-converter": {
     enabled: true,
   },
+  "protected-populate": {
+    enabled: true,
+    config: {
+      ["auto-populate"]: true,
+    },
+  },
 });
