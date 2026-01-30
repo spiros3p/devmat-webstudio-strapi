663ea859d8d7e7354e8e54a5bcba8fa8d3591719
spiros3p <spiros3p@gmail.com>
2026-01-30 18:32:03 +0000

    chore: add script to replace cms media url before commit

---
+++ scripts/fix-url.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const files = ['./.webstudio/data.json', './app/__generated__/[home]._index.tsx'];
+const search = 'http://localhost:1337';
+const replace = 'https://admin.devmat.spirosroadto.info';
+
+files.forEach(file => {
+  if (fs.existsSync(file)) {
+    const content = fs.readFileSync(file, 'utf8');
+    const updated = content.split(search).join(replace);
+    fs.writeFileSync(file, updated, 'utf8');
+    console.log(`✅ Updated URLs in ${file}`);
+  }
+});
